1632e51d6535ba3f6a86dc0bc6e264f546f62a4d
MdEahiaMondal <mdeahiamondal@gmail.com>
2019-12-07 18:09:59 +0600

    show all count product and others to dashboard

--- app/Http/Controllers/Backend/AdminController.php
+++ app/Http/Controllers/Backend/AdminController.php
@@ -2,7 +2,13 @@
 
 namespace App\Http\Controllers\Backend;
 
+use App\Brand;
+use App\Category;
+use App\Comment;
 use App\Http\Controllers\Controller;
+use App\Order;
+use App\Product;
+use App\Slider;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -18,7 +24,17 @@ class AdminController extends Controller
     }
 
     public function index(){
-        return view('backend.admin.dashboard.dashboard');
+
+        $users = User::count();
+        $categorys = Category::count();
+        $brands = Brand::count();
+        $sliders = Slider::count();
+        $products = Product::count();
+        $comments = Comment::count();
+        $orders = Order::count();
+
+        return view('backend.admin.dashboard.dashboard', compact('users',
+            'categorys', 'brands', 'sliders', 'products', 'orders', 'comments'));
     }
 
 
--- resources/views/backend/admin/dashboard/dashboard.blade.php
+++ resources/views/backend/admin/dashboard/dashboard.blade.php
@@ -1,4 +1,7 @@
 @extends('backend.admin.layout.admin_layout')
+
+@section('title', 'Dashboard')
+
 @section('admin_content')
     <ul class="breadcrumb">
         <li>
@@ -11,9 +14,68 @@
 
     <div class="row-fluid">
 
-        <p>Hey, This is Eahia khan. I am a php laravel Devolopper</p>
+        <a class="quick-button metro yellow span2">
+            <i class="icon-group"></i>
+            <p>Users</p>
+            <span class="badge">{{ $users }}</span>
+        </a>
 
-    </div>
 
+        <a class="quick-button metro green span2">
+            <i class="icon-barcode"></i>
+            <p>Categories</p>
+            <span class="badge">{{ $categorys }}</span>
+        </a>
+
+        <a class="quick-button metro green span2">
+            <i class="icon-barcode"></i>
+            <p>Brands</p>
+            <span class="badge">{{ $brands }}</span>
+        </a>
+
+        <a class="quick-button metro green span2">
+            <i class="icon-barcode"></i>
+            <p>Sliders</p>
+            <span class="badge">{{ $sliders }}</span>
+        </a>
+
+
+        <a class="quick-button metro green span2">
+            <i class="icon-barcode"></i>
+            <p>Products</p>
+            <span class="badge">{{ $products }}</span>
+        </a>
+
+        <a class="quick-button metro red span2">
+            <i class="icon-comments-alt"></i>
+            <p>Comments</p>
+            <span class="badge">{{ $comments }}</span>
+        </a>
+
+        <div class="clearfix"></div>
+
+    </div><!--/row-->
+
+    <br>
+    <br>
+    <br>
+
+    <div class="row-fluid">
+        <a class="quick-button metro blue span2">
+            <i class="icon-shopping-cart"></i>
+            <p>Orders</p>
+            <span class="badge">{{ $orders }}</span>
+        </a>
+
+        <a class="quick-button metro pink span2">
+            <i class="icon-envelope"></i>
+            <p>Messages</p>
+            <span class="badge">88</span>
+        </a>
+
+
+        <div class="clearfix"></div>
+
+    </div><!--/row-->
 
 @endsection
